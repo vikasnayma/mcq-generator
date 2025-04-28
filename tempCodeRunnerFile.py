9a9047d0f2d396f1351ad29d3a893bbe9356d500
Priyasharma297 <146953675+Priyasharma297@users.noreply.github.com>
2025-04-28 16:54:43 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-text = """
-# Marie Curie was a Polish-born physicist and chemist. She was the first woman to win a Nobel Prize in Physics.
-# Curie’s discovery of radium and polonium led to significant advancements in medical treatments. 
-# Her pioneering research on radioactivity fundamentally changed the field of atomic physics.
-# """
