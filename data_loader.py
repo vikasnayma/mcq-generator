53156ded92360388ab59c4c1b0fea65da84acc07
Priyasharma297 <146953675+Priyasharma297@users.noreply.github.com>
2025-04-28 16:49:25 +0530

    Delete data_loader.py

--- data_loader.py
+++
@@ -1,7 +0,0 @@
-from datasets import load_dataset
-
-def load_data():
-    dataset = load_dataset("cnn_dailymail", "3.0.0")
-    dataset["train"] = dataset["train"].select(range(80))  
-    dataset["validation"] = dataset["validation"].select(range(50))  
-    return dataset
